624904aaacbbc0c6193a2778ecbf313a2d8bcfe2
Manny <mannyschneck@gmail.com>
2017-02-19 15:37:37 -0600

    Add simple parallel implementation from lecture slides

--- opt_2dhisto.cu
+++ opt_2dhisto.cu
@@ -8,11 +8,42 @@
 
 void opt_2dhisto( /*define your own function parameters*/ )
 {
-    /* This function should only contain a call to the GPU 
-       histogramming kernel. Any memory allocations and
-       transfers must be done outside this function */
+        /* This function should only contain a call to the GPU
+           histogramming kernel. Any memory allocations and
+           transfers must be done outside this function */
 
 }
 
 /* Include below the implementation of any other functions you need */
 
+__global__ void opt_2dhisto_kernel(uint8_t *d_result, uint32_t *d_data, size_t data_sz){
+        const int globalTid = blockIdx.x * blockDim.x + threadIdx.x;
+
+        const int numThreads = blockDim.x * gridDim.x;
+
+        __shared__ unit s_Hist[NUM_BINS];
+
+        // 0 out shared memory for current block
+        for (int pos = threadIdx.x;
+             pos < NUM_BINS;
+             pos += blockDim.x){
+                s_Hist[pos] = 0;
+        }
+        __syncthreads();
+
+        for (int pos = globalTid;
+             pos < dataN;
+             pos+= numThreads){
+                uint32_t data4 = d_data[pos];
+                atomicAdd(s_Hist + (data4 >> 0) & 0xFFU, 1);
+                atomicAdd(s_Hist + (data4 >> 8) & 0xFFU, 1);
+                atomicAdd(s_Hist + (data4 >> 16) & 0xFFU, 1);
+                atomicAdd(s_Hist + (data4 >> 24) & 0xFFU, 1);
+        }
+
+        // merge partial histograms to global result
+        // atomic add will prevent cross-block races
+        for(int pos = threadIdx.x; pos < BIN_COUNT; pos += blockDim.x){
+                atomicAdd(d_result + pos, s_Hist[pos]);
+        }
+}
--- opt_2dhisto.h
+++ opt_2dhisto.h
@@ -1,9 +1,18 @@
 #ifndef OPT_KERNEL
 #define OPT_KERNEL
 
-void opt_2dhisto( /*Define your own function parameters*/ );
+#define HISTO_WIDTH  1024
+#define HISTO_HEIGHT 1
+#define NUM_BINS (HISTO_WIDTH * HISTO_HEIGHT)
+#define HISTO_LOG 10
+
+#define UINT8_MAX 255
+
+
+void opt_2dhisto(uint32_t *input[], size_t height, size_t width, uint8_t bins[HISTO_HEIGHT*HISTO_WIDTH]);
 
 /* Include below the function headers of any other functions that you implement */
 
+/* kernel is internal to implementation and as such, not exported in header */
 
 #endif
